69e09be3c49596b30be3e9485741ffca5927b618
zalayetha <zalayetha@gmail.com>
2026-06-16 08:58:57 +0700

    style: update Experience section

--- src/components/ui/ExperienceItem.tsx
+++ src/components/ui/ExperienceItem.tsx
@@ -6,15 +6,19 @@ interface ExperienceItemProps {
 
 export function ExperienceItem({ experience }: ExperienceItemProps) {
   return (
-    <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-      <div className="font-manrope font-semibold text-md text-gray-500">
-        {experience.company}
+    <div className="flex flex-col sm:flex-row sm:items-baseline gap-2 sm:gap-8">
+      <div className="flex-1">
+        <h4 className="font-newsreader text-lg font-semibold text-gray-950">
+          {experience.company}
+        </h4>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="font-manrope font-semibold text-md">{experience.role}</div>
-        <div className="font-manrope font-semibold text-md text-gray-500">
-          {experience.location} • {experience.period}
-        </div>
+      <div className="flex-1">
+        <p className="font-manrope font-medium text-gray-700">
+          {experience.role}
+        </p>
+        <p className="font-manrope text-sm text-gray-500">
+          {experience.location} · {experience.period}
+        </p>
       </div>
     </div>
   );
