f7c7d192b4993a9c48b964136aaa7aa9f30be617
zalayetha <zalayetha@gmail.com>
2026-06-17 08:24:32 +0700

    refactor: convert all react components to astro components

--- src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { Hero as HomeHero } from "./home/Hero";
-export { Hero as AboutHero } from "./about/Hero";
-export { Academic } from "./about/Academic";
-export { Contact } from "./home/Contact";
-export { Experience } from "./about/Experience";
-export { Projects } from "./Projects";
-export { Skills } from "./Skills";
